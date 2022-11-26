b2c6bcdac71640aa56a6c8378d35e39537bd13b3
tradeshow01 <110342169+tradeshow01@users.noreply.github.com>
2022-11-26 19:00:10 +0530

    Firebase Create Data and Get Data

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -346,6 +346,7 @@
   };
 
   // Sidebar Toggle
+  
 
   $(".btn").click(function () {
     $(this).toggleClass("click");
@@ -372,6 +373,29 @@
     });
   });
   // End Sidebar Toggle
+
+  const deafultValue = 3; /* set Active: index start from zero */
+
+/* 2/2 change content base on select */
+$(document).ready(function(){
+  /* set the deafult select-menu value */
+  $('#search-select').prop('selectedIndex', deafultValue); 
+  //hides dropdown content
+  $(".dropdown-content-bond").hide();
+  //unhides index option content
+  $(".dropdown-content-bond").eq( deafultValue ).show();
+  //listen to dropdown for change
+  $("#search-select").change(function(){
+    //rehide content on change
+    $('.dropdown-content-bond').hide();
+    //unhides current item
+    var currentIndex = $(this).prop('selectedIndex');
+    console.log(currentIndex);
+    $(".dropdown-content-bond").eq( currentIndex ).show(); 
+  });
+  
+});
+
 
   var popupVideo = function () {
     if ($().magnificPopup) {
--- src/App.js
+++ src/App.js
@@ -4,29 +4,20 @@ import Header from './Component/Header';
 import Contact from './Contact';
 import Register from './Register';
 import Footer from './Component/Footer';
-import {initializeApp} from 'firebase/app';
+import BasicInformation from './Basic Information'
 import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';  
-
-
-
+// import { getFirestore } from "firebase/firestore";
+import Login from './Login'
 function App() {
-  const firebaseConfig = {
-    apiKey: "AIzaSyB0f8xXiSfISh9ovHQcJzwS7Xt_Fs1hiBQ",
-    authDomain: "mvp--login.firebaseapp.com",
-    projectId: "mvp--login",
-    storageBucket: "mvp--login.appspot.com",
-    messagingSenderId: "558551604106",
-    appId: "1:558551604106:web:dfd5384f3a22b6837c4673",
-    measurementId: "G-9YJNN0HPKJ"
-  };
-  const app = initializeApp(firebaseConfig);
+
   return (
     <>
-
        <Router>  
        <Header/>
-          
            <Routes>  
+
+           <Route exact path="/Login"  element={< Login />}></Route>
+                 <Route exact path='/Userinformation'  element={< BasicInformation />}></Route>  
                  <Route exact path='/' element={< Home />}></Route>  
                  <Route exact path='/About_us' element={< About_Us />}></Route>  
                  <Route exact path='/Register' element={< Register />}></Route>  
